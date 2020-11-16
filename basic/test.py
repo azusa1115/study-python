395a32532623f76f05525d43f11ddc81af2b1bf5
azusa1115 <kato.azusa.1115@gmail.com>
2020-11-16 11:51:10 +0900

    test

---
+++ basic/test.py
@@ -0,0 +1,57 @@
+
+
+print("あいう\
+  eojikd")
+
+
+import tkinter
+tkinter.Label(None,text="Hello").pack()
+
+
+print(1+2)
+
+import calendar
+print(calendar.month(2020,3))
+
+
+print(1+2)
+print(3+4)
+print(4+5)
+print("abc")
+print("")
+print("abc+cde")
+print("abc+cde+def+hij")      
+print("abc"*3)
+print("abc"+str(123*234))
+print("こんにちは")
+
+a = 1
+
+b = 2
+
+C="abc"
+
+print( a + b )
+for a in [1,2,3,4,5]:
+  print(a)
+  print("こんにちは")
+
+for a in range(10):
+  print(a)
+  print("こんにちは")
+
+
+
+total = 0
+a = 1
+while total <= 50:
+  total = total + a
+  a = a +1
+print(total)
+
+
+for a in range(1,10+1):
+  if a <= 5:
+    print("小さいです")
+  else:
+    print("大きいです")
