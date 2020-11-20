d45241f47fa4ed60711fff27202b59ee22ce60e5
azusa1115 <kato.azusa.1115@gmail.com>
2020-11-20 16:36:22 +0900

    YouTube

--- basic/chapter5-2.py
+++ basic/chapter5-2.py
@@ -47,5 +47,25 @@ print(b[1])
 print(b[2])
 print(b[3])
 
+hit = 0
+if a[0] == int(b[0]):
+  hit = hit + 1
+if a[1] == int(b[1]):
+  hit = hit + 1
+if a[2] == int(b[2]):
+  hit == hit + 1
+if a[3] == int(b[3]):
+  hit = hit + 1
 
+#forでも書く事ができる
 
+hit = 0
+for i in range(4)
+  if a[i] == int(b[i]):
+    hit = hit + 1
+
+blow = 0
+for i in range(4)
+  if int(b[0]) == a[i]):
+    blow = blow + 1
+    break
