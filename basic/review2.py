d807745a315ea28ddba624170e4048714a4a4682
azusa1115 <kato.azusa.1115@gmail.com>
2020-11-21 23:38:05 +0900

    復讐

---
+++ basic/review2.py
@@ -0,0 +1,31 @@
+# coding:utf-8
+import random
+
+a = random.randint( 0 , 9 )
+print(a)
+
+b = int(input("数を入れてね>"))
+print(b)
+
+if a == b:
+  print("あたり")
+else:
+  print("はずれ")
+
+a1 = random.randint( 0 , 9 )
+a2 = random.randint( 0 , 9 )
+a3 = random.randint( 0 , 9 )
+a4 = random.randint( 0 , 9 )
+
+print((str[a0]) + (str[a1]) + (str[a2}) + (str[a3]))
+
+import random
+
+b = input(”数を入れてね」”)
+print(b[0])
+print(b[1])
+print(b[2])
+print(b[3]
+
+
+
