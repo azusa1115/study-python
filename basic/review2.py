99aa03d5563d893febc5f8a4f868bc5e4929c551
azusa1115 <kato.azusa.1115@gmail.com>
2020-11-23 23:29:11 +0900

    復讐2

--- basic/review2.py
+++ basic/review2.py
@@ -4,28 +4,34 @@ import random
 a = random.randint( 0 , 9 )
 print(a)
 
-b = int(input("数を入れてね>"))
-print(b)
+b = int(input("数を入れてね＞"))
 
 if a == b:
   print("あたり")
 else:
   print("はずれ")
 
-a1 = random.randint( 0 , 9 )
-a2 = random.randint( 0 , 9 )
-a3 = random.randint( 0 , 9 )
-a4 = random.randint( 0 , 9 )
+#a1 = random.randint( 0 , 9 )
+#a2 = random.randint( 0 , 9 )
+#a3 = random.randint( 0 , 9 )
+#a4 = random.randint( 0 , 9 )
 
-print((str[a0]) + (str[a1]) + (str[a2}) + (str[a3]))
+#print(str(a1)+str(a2)+str(a3)+str(a4))
 
-import random
+a = [random.randint(0,9),
+     random.randint(0,9),
+     random.randint(0,9),
+     random.randint(0,9)]
+print(str(a[0])+str(a[1])+str(a[2])+str(a[3]))
 
-b = input(”数を入れてね」”)
+b = input("数を入れてね>")
 print(b[0])
 print(b[1])
 print(b[2])
-print(b[3]
+print(b[3])
 
+b = input("数を入れてね＞")
+if len(b)!=4:
+  print("４桁の数字を入力してください")
 
 
